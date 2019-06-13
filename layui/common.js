3980aef3261b225ef89bbabd8fd5c7d3a2522939
yangjy <664243624@qq.com>
2019-06-13 08:55:30 +0800

    提交ajax组件

--- layui/common.js
+++ layui/common.js
@@ -1,3 +1,23 @@
 var requestPath = "http://39.104.97.6:8080";
 
 var page = 1;
+
+layui.define(['jquery'], function(exports) {
+	var $ = layui.jquery;
+	var obj = {
+		ajax: function(url, type, dataType, data, callback) {
+			$.ajax({
+				url: url,
+				type: type,
+				dataType: dataType,
+				headers: {
+					"Content-Type":"application/json; charset=utf-8"
+				},
+				data: data,
+				success: callback,
+			});
+		}
+	};
+	//输出接口
+	exports('common', obj);
+});
