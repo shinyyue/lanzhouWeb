fb471b71582e95c79416f0549442a0d0bfc5f540
yangjy <664243624@qq.com>
2019-06-14 13:57:05 +0800

    修复BUG，提交联调的学期设置列表和添加接口

--- layui/common.js
+++ layui/common.js
@@ -1,29 +1,29 @@
-var requestPath = "http://39.104.97.6:8080";
+//var requestPath = "http://39.104.97.6:8080";
+//
+//var page = 1;
 
-var page = 1;
-
-layui.define(['jquery'], function(exports) {
-	var $ = layui.jquery;
-	var obj = {
-		ajax: function(url, type, dataType, data, callback) {
-			$.ajax({
-				url: url,
-				type: type,
-				dataType: dataType,
-				headers: {
-					"Content-Type": "application/json; charset=utf-8"
-				},
-				data: data,
-				success: callback,
-			});
-		}
-	};
-	//输出接口
-	exports('common', obj);
-});
-
-layui.config({
-	base: './layui/' //自定义layui组件的目录
-}).extend({ //设定组件别名
-	common: 'common',
-});
+//layui.define(['jquery'], function(exports) {
+//	var $ = layui.jquery;
+//	var obj = {
+//		ajax: function(url, type, dataType, data, callback) {
+//			$.ajax({
+//				url: url,
+//				type: type,
+//				dataType: dataType,
+//				headers: {
+//					"Content-Type": "application/json; charset=utf-8"
+//				},
+//				data: data,
+//				success: callback,
+//			});
+//		}
+//	};
+//	//输出接口
+//	exports('common', obj);
+//});
+//
+//layui.config({
+//	base: './layui/' //自定义layui组件的目录
+//}).extend({ //设定组件别名
+//	common: 'common',
+//});
