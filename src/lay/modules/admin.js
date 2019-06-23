5ad30154f8e8a07662daf1155ea821171dd01417
岳乃杉 <763839213@qq.com>
2019-06-23 11:39:06 +0800

    课程导读

--- src/lay/modules/admin.js
+++ src/lay/modules/admin.js
@@ -26,6 +26,49 @@ layui
         self.routeLeaveFunc = null
         self.loginToken = null
 
+        Date.prototype.format = function (fmt) { //author: meizz  
+            var o = {
+                "M+": this.getMonth() + 1, //月份  
+                "d+": this.getDate(), //日  
+                "h+": this.getHours(), //小时  
+                "m+": this.getMinutes(), //分  
+                "s+": this.getSeconds(), //秒   
+                "q+": Math.floor((this.getMonth() + 3) / 3), //q是季度
+                "S": this.getMilliseconds() //毫秒  
+            };
+            if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+            for (var k in o)
+                if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            return fmt;
+        }
+
+        String.prototype.format = function (args) {
+            var result = this;
+            if (arguments.length > 0) {
+                if (arguments.length == 1 && typeof (args) == "loginTime") {
+                    for (var key in args) {
+                        if (args[key] != undefined) {
+                            var reg = new RegExp("({" + key + "})", "g");
+                            result = result.replace(reg, args[key]);
+                        }
+                    }
+                } else {
+                    for (var i = 0; i < arguments.length; i++) {
+                        if (arguments[i] != undefined) {
+                            //var reg = new RegExp("({[" + i + "]})", "g");//这个在索引大于9时会有问题  
+                            var reg = new RegExp("({)" + i + "(})", "g");
+                            result = result.replace(reg, arguments[i]);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        self.dateFormat = function (value) {
+            return value ? new Date(value * 1000).format("yyyy-MM-dd hh:mm:ss") : "";
+        }
+
         self.routeLeave = function (callback) {
             this.routeLeaveFunc = callback
         }
